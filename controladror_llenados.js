5870a0ffa21deb035f9a08164f4abeccb16fbb3f
501 <501@academicos.cesde.edu.co>
2022-01-20 09:38:20 -0500

    secrea un cambio

---
+++ controladror_llenados.js
@@ -0,0 +1,13 @@
+let productos=[
+    {nombre:"aguardiente tapa roja",tamano:"1/2 botella",precio:200000,foto:"img/roja.png"},
+    {nombre:"aguardiente tapa azul",tamano:"1/2 botella", precio:220000,foto:"img/roja.png"},
+    {nombre:"teqila don julio",tamano:"1 botella grande", precio:300000,foto:"img/roja.png"},
+    {nombre:"cerveza agilla",tamano:"1 botella", precio:8000,foto:"img/roja.png"},
+    {nombre:"cerveza pilsen",tamano:"1 botella", precio:7000,foto:"img/roja.png"},
+    {nombre:"ron medellin",tamano:"1/2 botella", precio:220000,foto:"img/roja.png"},
+    {nombre:"ron añejo 220 años de caldas",tamano:"1/2 botella", precio:100000,foto:"img/roja.png"},
+    {nombre:"agua",tamano:"1 botella", precio:4000,foto:"img/roja.png"},
+    {nombre:"soda",tamano:"1 botella", precio:6000,foto:"img/roja.png"},
+    {nombre:"gatorade",tamano:"1 botella", precio:9000,foto:"img/roja.png"},
+    
+]
